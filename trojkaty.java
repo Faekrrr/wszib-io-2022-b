e02cc11c6a0b15126df369cd130746d987907f62
khalczak <kamilhal5336@gmail.com>
2022-06-18 16:10:54 +0200

    Optymalizacja: uwspólnienie kodu

--- trojkaty.java
+++ trojkaty.java
@@ -1,22 +1,48 @@
 /**
+ 
 * To jest program do rozpoznawania trójkąta.
 */
-
+ 
 class Trojkaty {
     /**
-     * Główna funkcja programu. 
+    * Funkcja zwraca ile par odcinków trójkąta ma jednakową długość.
+    * @param {float} a - Długość pierwszego boku.
+    * @param {float} b - Długość drugiego boku.
+    * @param {float} c - Długość trzeciego boku.
+    */
+    public static int ileJednakowych(float a, float b, float c){
+        int jednakowe = 0; // tutaj zliczamy ile jest jednakowych bokow
+        if (a == b) {
+            jednakowe++;
+        }
+        if (b == c) {
+            jednakowe++;
+        }
+        return jednakowe;
+    }
+    /**
+     * Główna funkcja programu.
      * @param {float} a - Długość pierwszego boku.
      * @param {float} b - Długość drugiego boku.
      * @param {float} c - Długość trzeciego boku.
      */
     public static void jakiTrojkat(float a, float b, float c){
+        int jednakoweDlugosci = ileJednakowych(a, b, c); // tutaj zliczamy ile jest jednakowych bokow
+        if (jednakoweDlugosci > 1) {
+            System.out.println("Trójkąt równoboczny"); 
+        } else if (jednakoweDlugosci > 0) {
+            System.out.println("Trójkąt równoramienny");
+        }
          if (a == b && b == c) {
              System.out.println("Trójkąt równoboczny"); 
          }
          // TODO: tutaj trzeba bedzie dopisac inne przypadki
-	if (a == b || b == c || a == c) {
-                    System.out.println("Trójkąt równoramienny");
-        }
+        if (a == b && b == c && a == c) {
+            System.out.println("Trójkąt równoboczny");
+            }
+        if (a == b || b == c || a == c) {
+            System.out.println("Trójkąt równoramienny");
+            }
     }
     /** Wyświetla ekran pomocy */
     public static void pomoc(){
@@ -24,7 +50,11 @@ class Trojkaty {
         System.out.println("Program do rozpoznawania rodzaju trójkąta");
         System.out.println("Uruchom z trzema argumentami liczbowymi - długość boków trójkąta");
     }
-    /** Glowna funkcja */
+    /** Glowna funkcja
+    * @param {float} a - Długość pierwszego boku.
+    * @param {float} b - Długość drugiego boku.
+    * @param {float} c - Długość trzeciego boku.
+    */
     public static void main(String... args) {
         if (args.length != 3) {
             pomoc();
@@ -33,11 +63,11 @@ class Trojkaty {
         float a = Float.valueOf(args[0]);
         float b = Float.valueOf(args[1]);
         float c = Float.valueOf(args[2]);
-
-        if (a < 0 || b < 0 || c < 0) {
-        	System.out.println("Długości boków trójkąta muszą być nieujemne!");
-		System.exit(2);
-        	}        
-        jakiTrojkat(a, b, c);
+ 
+         if (a < 0 || b < 0 || c < 0) {
+               System.out.println("Długości boków trójkąta muszą być nieujemne!");
+               System.exit(2);
+                }
+                jakiTrojkat(a, b, c);
     }
 }
